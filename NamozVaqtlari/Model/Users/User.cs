9eb866404c0eacb9ec6b4d37527edfa9ac283a6f
farobiy <farobiyfrontend@gmail.com>
2024-09-18 00:00:33 +0500

    Create User Model and Roles

---
+++ NamozVaqtlari/Model/Users/User.cs
@@ -0,0 +1,14 @@
+﻿namespace NamozVaqtlari.Model.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public bool IsActived { get; set; }
+        public int SmsCode { get; set; }
+        public string? Token { get; set; }
+        public List<UserRole>? Role { get; set; }
+    }
+}
