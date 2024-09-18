956b0ec08961c44e9fcab69a373836632b75f6aa
farobiy <farobiyfrontend@gmail.com>
2024-09-18 10:14:22 +0500

    Added repository

---
+++ NamozVaqtlari/Controllers/Auth/SignController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NamozVaqtlari.Controllers.Auth
+{
+    public class SignController:ControllerBase
+    {
+        public SignController() { }
+
+
+    }
+}
