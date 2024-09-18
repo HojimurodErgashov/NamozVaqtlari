0fbf4cdf8092c4e859ba04fb7484663de95d8fd4
Hojimurod2003 <ergashovhojimurod4@gmail.com>
2024-09-18 22:46:54 +0500

    Created Folders , class and interface but not write Algoritm. i do not have Twilio Accaunt.

---
+++ NamozVaqtlari/Services/SmsService/SmsService.cs
@@ -0,0 +1,15 @@
+﻿namespace NamozVaqtlari.Services.SmsService
+{
+    public class SmsService : ISmsService
+    {
+        public int GenerateSmsCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendSmsToPhoneNumber(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
